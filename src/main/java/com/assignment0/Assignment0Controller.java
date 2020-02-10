6596a75a370615db3e09eaee0fd3e9bbc306f8e5
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-10 14:29:27 -0500

    Added new index.html and created controller class with index mapping.

---
+++ src/main/java/com/assignment0/Assignment0Controller.java
@@ -0,0 +1,15 @@
+package com.assignment0;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Assignment0Controller {
+	
+	@RequestMapping("/index")
+	public String index() {
+		
+		return "index";
+	}
+
+}
