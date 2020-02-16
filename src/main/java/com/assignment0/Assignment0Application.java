8756fc7252e8ecb2b65af4ed5f941b000d7f7c36
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-16 10:56:45 -0500

    Added new request mappings for upload pages, also added an error page.

--- src/main/java/com/assignment0/Assignment0Application.java
+++ src/main/java/com/assignment0/Assignment0Application.java
@@ -1,12 +1,17 @@
 package com.assignment0;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.assignment0", "com.assignment0"})
 public class Assignment0Application {
 
 	public static void main(String[] args) {
+		new File(Assignment0Controller.uploadDirectory).mkdir();
 		SpringApplication.run(Assignment0Application.class, args);
 	}
 
--- src/main/java/com/assignment0/Assignment0Controller.java
+++ src/main/java/com/assignment0/Assignment0Controller.java
@@ -1,15 +1,49 @@
 package com.assignment0;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class Assignment0Controller {
+	public static String uploadDirectory = System.getProperty("user.dir")+ "/uploads";
 	
 	@RequestMapping("/index")
 	public String index() {
 		
 		return "index";
 	}
+	
+	@RequestMapping("/")
+	public String UploadPage(Model model) {
+		return "uploadview";
+	}
+	
+	@RequestMapping("/upload")
+	public String upload(Model model, @RequestParam("files") MultipartFile[] files) {
+		StringBuilder fileNames = new StringBuilder();
+		for(MultipartFile file: files) {
+			Path fileNameAndPath = Paths.get(uploadDirectory, file.getOriginalFilename());
+			fileNames.append(file.getOriginalFilename());
+			try {
+				Files.write(fileNameAndPath, file.getBytes());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		}
+		model.addAttribute("msg", "Successfully uploaded file" + fileNames.toString());
+		return "uploadstatusview";
+	}
+	
+
 
 }
